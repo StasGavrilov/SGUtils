e60678cda7bf29a3b44b49895ddf37eee8cb605a
Stas's mac <sgavrilov@infinidat.com>
2024-05-23 09:58:27 +0300

    Finally starting

---
+++ src/components/Head/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div>
+      <span>Stas</span>
+      <span>Bot</span>
+      <span>.</span>
+    </div>
+  )
+}
---
+++ src/components/hourds-to-days/HoursToDays.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HoursToDays() {
+  return (
+    <div>
+      HoursToDays
+    </div>
+  )
+}

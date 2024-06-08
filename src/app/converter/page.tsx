1ee90541259646f79f04d51e41af56191be44cdd
stasgavrilov <stasgavrilov485@gmail.com>
2024-06-08 08:33:03 +0300

    improved app

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import React, { useState } from 'react'
+
 import Box from "@/components/Box/Box"
 import Table from "@/components/Table/Table"
 import NumberInput from "./NumberInput"
 import UnitSelection from "./UnitSelection"
 import Result from "./Result"
+import Counting from './Counting'
 
 export default function Converter() {
   const [value, setValue] = useState<number | ''>('')
@@ -13,21 +15,18 @@ export default function Converter() {
 
   const headers = ['Input', 'Unit', 'Status', 'Result']
   const data = [
-    [
-      <NumberInput value={value} onValueChange={setValue} />,
-      <UnitSelection selectedUnit={selectedUnit} onUnitChange={setSelectedUnit} />,
-      <span>{value ? 'Counting..' : 'Waiting'}</span>,
-      <Result value={value} unit={selectedUnit} />,
-    ],
+    <NumberInput value={value} onValueChange={setValue} />,
+    <UnitSelection selectedUnit={selectedUnit} onUnitChange={setSelectedUnit} />,
+    <Counting value={value} />,
+    <Result value={value} unit={selectedUnit} />,
   ]
-
+  
   return (
     <Box title="Converter">
       <Table headers={headers} data={data} />
 
       <div className='py-24'>
         <h1 className='text-xl underline underline-offset-2'>Description:</h1>
-
         <p className='text-lg'>This application converts hours into weeks, months, and years, assisting in determining the age of drives when analyzing faulty drives in field or lab systems.</p>
       </div>
     </Box>
--- src/app/progress/page.tsx
+++ src/app/progress/page.tsx
@@ -2,15 +2,18 @@
 
 import React, { useEffect, useState } from 'react'
 import { supabase } from '@/../supabaseClient'
-import Box from '@/components/Box/Box'
 
+import Box from '@/components/Box/Box'
+import Loading from '@/components/Loading/Loading'
+import Table from '@/components/Table/Table'
+import {headers} from './headers'
 interface ProgressData {
   id: number
   title: string
 }
 
 export default function Progress() {
-  const [fetchError, setFetchError] = useState<string | null>(null)
+  const [fetchError, setFetchError] = useState<string | null>(null) //NOT ADDED YET.
   const [progress, setProgress] = useState<ProgressData[] | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(true)
 
@@ -36,13 +39,12 @@ export default function Progress() {
     fetchData()
   }, [])
 
+  const data = [1,2,3,4] //PLACEHOLDERS
+
   return (
-    <Box title='DATABASE'>
-      {isLoading && <p>Loading...</p>}
-      {fetchError && <p>{fetchError}</p>}
-      {progress && progress.map(prog => (
-        <div key={prog.id}>{prog.title}</div>
-      ))}
+    <Box title='Progress'>
+      <button>Add</button>
+      {isLoading ? <Loading /> : <Table headers={headers} data={data} />}
     </Box>
   )
 }

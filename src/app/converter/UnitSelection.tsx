a4da08e99ba7ea1ceb574dfbe542c992e8fefb6b
Stas's mac <sgavrilov@infinidat.com>
2024-06-17 15:00:48 +0300

    improved conventer

--- src/app/converter/UnitSelection.tsx
+++ src/app/converter/UnitSelection.tsx
@@ -1,22 +1,25 @@
 interface UnitSelectionProps {
+  label: string
   selectedUnit: string
   onUnitChange: (newUnit: string) => void
 }
 
-export default function UnitSelection({ selectedUnit, onUnitChange }: UnitSelectionProps) {
-  const units = ['Days', 'Weeks', 'Months', 'Years']
+export default function UnitSelection({ label, selectedUnit, onUnitChange }: UnitSelectionProps) {
+  const units = ['Hours', 'Days', 'Weeks', 'Months', 'Years']
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     onUnitChange(e.target.value)
   }
 
   return (
-    <select value={selectedUnit} onChange={handleChange}>
-      {units.map((unit, index) => (
-        <option key={index} value={unit}>
-          {unit}
-        </option>
-      ))}
-    </select>
+    <div>
+      <select value={selectedUnit} onChange={handleChange}>
+        {units.map((unit, index) => (
+          <option key={index} value={unit}>
+            {unit}
+          </option>
+        ))}
+      </select>
+    </div>
   )
 }

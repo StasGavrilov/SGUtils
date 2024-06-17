f6e5f60c0696eb4a6dd902b7efe4f88252157242
Stas's mac <sgavrilov@infinidat.com>
2024-06-17 15:16:41 +0300

    improved table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -13,12 +13,18 @@ export default function Table({ headers, data }: ITable) {
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full bg-white border border-gray-300 table-fixed">
+        <colgroup>
+          {headersToDisplay.map((_, index) => (
+            <col key={index} style={{ width: `${100 / headersToDisplay.length}%` }} />
+          ))}
+        </colgroup>
+
         <thead className="bg-gray-100">
           <tr>
             {headersToDisplay.map((header, index) => (
               <th
                 key={index}
-                className="py-2 px-4 border-b border-r text-center w-auto"
+                className="py-2 px-4 border-b border-r text-center"
               >
                 {header}
               </th>
@@ -32,7 +38,7 @@ export default function Table({ headers, data }: ITable) {
               {headersToDisplay.map((header, cellIndex) => (
                 <td
                   key={cellIndex}
-                  className="py-2 px-4 border-b border-r text-center w-auto"
+                  className="py-2 px-4 border-b border-r text-center"
                 >
                   {row[header]}
                 </td>

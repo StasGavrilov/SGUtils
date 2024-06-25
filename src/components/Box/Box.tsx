d89757843778dc1129324cb7d24561b28ad43a9b
Stas's mac <sgavrilov@infinidat.com>
2024-06-25 06:16:56 +0300

    fixed box

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -5,9 +5,8 @@ interface ITitle {
 
 export default function Box({ title, children }: ITitle) {
   return (
-    <div className="p-4">
+    <div className="p-4 lg:ml-0 ml-[-6rem] transition-all duration-500 ease-in-out">
       <h2 className='text-2xl'>{title}</h2>
-
       <main className="p-2 mt-4">
         {children}
       </main>

2058fccd8b66c38f2337ac61bf6d48384c42e945
Stas's mac <sgavrilov@infinidat.com>
2024-06-27 13:08:51 +0300

    hot fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 import Image from 'next/image'
-
 import Box from "@/components/Box/Box"
 
 export default function Home() {
   return (
     <Box title='Welcome to Stasg-bot application.'>
-      <div className='flex justify-center items-center min-h-screen'>
+      <div className='flex flex-col lg:flex-row justify-center items-center min-h-screen'>
         <Image
           src={'/pictures/pro.jpg'}
           alt={'emblem'}
-          width={350}
-          height={600}
-          className='rounded'
+          width={250}
+          height={400}
+          className='rounded lg:w-[350px] lg:h-[500px]'
         />
-
-        <div className='py-10 px-24 text-center'>
+        <div className='py-10 px-8 lg:px-24 text-center'>
           <p className='text-3xl'>I am Stas Gavrilov.</p>
           <p className='text-2xl'>Working at Infinidat.</p>
           <p className='text-1xl'>As Hardware Integration Engineer.</p>

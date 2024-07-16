87540f208e0c549cb244a04fbfba354627bff5be
Stas's mac <sgavrilov@infinidat.com>
2024-07-16 14:25:07 +0300

    wip

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -7,29 +7,31 @@ import Table from "@/components/Table/Table"
 import NumberInput from "./NumberInput"
 import UnitSelection from "./UnitSelection"
 import Result from "./Result"
-import Counting from './Counting'
 
 export default function Converter() {
   const [value, setValue] = useState<number | ''>('')
   const [fromUnit, setFromUnit] = useState<string>('Hours')
   const [toUnit, setToUnit] = useState<string>('Years')
 
-  const headers = ['Input', 'From', 'To', 'Status', 'Result']
+  const headers = ['Input', 'From', 'To', 'Result']
   const data = [
     {
       Input: <NumberInput value={value} onValueChange={setValue} />,
       From: <UnitSelection label="From" selectedUnit={fromUnit} onUnitChange={setFromUnit} />,
       To: <UnitSelection label="To" selectedUnit={toUnit} onUnitChange={setToUnit} />,
-      Status: <Counting value={value} />,
       Result: <Result value={value} fromUnit={fromUnit} toUnit={toUnit} />,
     }
   ]
 
   return (
     <Box title="Time Converter">
-      <Table headers={headers} data={data} />
+      <div className='flex justify-center items-center h-screen'>
+        <div className='absolute top-[40%] w-[75%]'>
+          <Table headers={headers} data={data} />
+        </div>
+      </div>
 
-      <div className='py-24'>
+      <div className='absolute bottom-0 w-full py-24'>
         <h1 className='text-xl underline underline-offset-2'>Description:</h1>
         <p className='text-lg'>This application converts between various time units, assisting in determining the age of drives when analyzing faulty drives in field or lab systems.</p>
       </div>
